5824e8346ba51a06b9a75b895e2f20df8a2cf49f
alienware by casablanca <evaseemefly@126.com>
2017-08-16 22:26:24 +0800

    控制器向视图返回特殊类型

--- myfirstsite/test_tmpl/learn/views.py
+++ myfirstsite/test_tmpl/learn/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def home(request):
+#     return render(request,'home.html')
+
+# 返回string类型的字符串
+# def home(request):
+#     string =u"django"
+#     return render(request,'home.html',{'string':string})
+
 def home(request):
-    return render(request,'home.html')
+    TutorialList=["Html","css","jquery","python"]
+    return render(request,'home.html',{'TutorialList':TutorialList})
+
+def home(request):
+    info_dict={'site':u'drno','content':u'内容测试内容测试'}
+    return render(request,'home.html',{'info_dic':info_dict})
+
+
